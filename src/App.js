081ec6eb3f872b991a60101591e3273e68f0da74
Mostafa <engmostafa51999@gmail.com>
2023-06-02 14:56:57 +0300

    Add Simple Router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux';
 
 function App() {
   return (
+    <Provider>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+
+
+
+<Routes>
+  <Route path='/product' element=""/>
+  <Route path='/' element=""/>
+  <Route path='/about' element=""/>
+  <Route path='/favourite' element=""/>
+  <Route path='/movie' element="">
+  
+  </Route>
+  <Route path='/details/:id' element=""/>
+</Routes>
+ 
+ 
     </div>
+    </Provider>
   );
 }
 
