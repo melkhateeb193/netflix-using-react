334c72bd0971264cedb18282e395012d31fad949
melkhateeb193 <mostafamagdy8800@gmail.com>
2023-06-02 17:18:43 +0300

    first two pages

---
+++ src/component/userprofile/userprofile.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import './userprofile.css'
+function Userprofile() {
+    return (
+        <div className="container p-5 d-flex flex-column justify-content-center align-items-center text-center">
+        <h1 className="mt-5 mb-2">who's watching?</h1>
+        <div className="container row p-5 w-100 ">
+          <div className="col-md text-center ">
+            <div className="box">
+              <a href="/Home/index.html">
+                {" "}
+                <img src="images/smile1.png" alt="" />
+              </a>
+            </div>
+            <p>
+              <a href="/Home/index.html">Mostafa Magdy</a>
+            </p>
+          </div>
+          <div className="col-md text-center">
+            <div className="box">
+              <a href="/Home/index.html">
+                {" "}
+                <img src="images/smile2.png" alt="" />{" "}
+              </a>
+            </div>
+            <p>
+              <a href="/Home/index.html">Abdelrahman alaa</a>
+            </p>
+          </div>
+          <div className="col-md text-center">
+            <div className="box">
+              <a href="/Home/index.html">
+                {" "}
+                <img src="images/smile3.png" alt="" />{" "}
+              </a>
+            </div>
+            <p>
+              <a href="/Home/index.html">karem awad</a>
+            </p>
+          </div>
+          <div className="col-md text-center">
+            <div className="box">
+              <a href="/Home/index.html">
+                {" "}
+                <img src="images/smile4.png" alt="" />{" "}
+              </a>
+            </div>
+            <p>
+              <a href="/Home/index.html">Mostafa ali</a>
+            </p>
+          </div>
+          <div className="col-md text-center">
+            <div className="boxAdd  d-flex justify-content-center align-items-center">
+              <i className="fa-solid fa-circle-plus fa-5x" />
+            </div>
+            <p>Add Profiles</p>
+          </div>
+        </div>
+        <button className="btn btn-outline-dark text-white">Manage profiles</button>
+      </div>
+    )
+}
+
+export default Userprofile
