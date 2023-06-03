feb63955fd2a7814b8ade4c43c2ae389cb3cfd15
melkhateeb193 <mostafamagdy8800@gmail.com>
2023-06-03 22:39:14 +0300

    upload the slider component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@
 // import Mainpagebfl from './component/mainpagebeforlogin/mainpagebfl';
 // import Login from './component/login/login';
 // import Userprofile from './component/userprofile/userprofile';
-import Homepage from './component/homepage/homepage';
+import SimpleSlider from './component/slider/slider';
 
 function App() {
   return (
-    <div className="App">
-    <Homepage/>
+    <div className="App bg-dark">
+    <SimpleSlider/>
+    <SimpleSlider/>
 
     </div>
     // <Router>
