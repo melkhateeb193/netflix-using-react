532220beff8ffcac9eb2fed456b48fb9a2b8ae39
abdo <abdoalaaabdo17@gmail.com>
2023-06-09 22:28:05 +0300

    cards

--- src/component/slider/slider.jsx
+++ src/component/slider/slider.jsx
@@ -4,6 +4,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Card from "react-bootstrap/Card";
 import { PropaneSharp } from "@mui/icons-material";
+import "./slider.css";
 
 const Responsive = (props) => {
   const settings = {
@@ -11,7 +12,36 @@ const Responsive = (props) => {
     infinite: true,
     centerPadding: "100px",
     slidesToShow: 6,
+    slidesToScroll: 1,
     swipeToSlide: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+    // nextArrow: <SampleNextArrow />,
+    //   prevArrow: <SamplePrevArrow />,
     afterChange: function (index) {
       console.log(
         `Slider Changed to: ${index + 1}, background: #222; color: #bada55`
@@ -20,61 +50,61 @@ const Responsive = (props) => {
   };
 
   return (
-    <div className="container-fluid px-5 py-3 Slider">
+    <div className="container-fluid px-5 py-3 SliderContainer">
       <div className="SliderContainer">
         <h5>{props.title}</h5>
       </div>
       <Slider {...settings}>
-        <div style={{ margin: "0 5px" }}>
-          <Card style={{ width: "15rem" }}>
+        <div className="slider__element">
+          <Card className="slider__element--child">
             <Card.Img
               variant="top"
               src="http://occ-0-6661-55.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABdMUFn_qx4wIfV8BL5FfRORBtxn9xZduOUYHpID0GPRUPkxlQl-MSCWXLYzNiIhTbQIiu4fjJhbFJOpgxmmeskNzuhAJXWNPfKU.webp?r=c6c"
             />
           </Card>
         </div>
-        <div style={{ margin: "0 5px" }}>
-          <Card style={{ width: "15rem" }}>
+        <div className="slider__element">
+          <Card className="slider__element--child">
             <Card.Img
               variant="top"
               src="http://occ-0-6661-55.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABdMUFn_qx4wIfV8BL5FfRORBtxn9xZduOUYHpID0GPRUPkxlQl-MSCWXLYzNiIhTbQIiu4fjJhbFJOpgxmmeskNzuhAJXWNPfKU.webp?r=c6c"
             />
           </Card>
         </div>
-        <div style={{ margin: "0 5px" }}>
-          <Card style={{ width: "15rem" }}>
+        <div className="slider__element">
+          <Card className="slider__element--child">
             <Card.Img
               variant="top"
               src="http://occ-0-6661-55.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABdMUFn_qx4wIfV8BL5FfRORBtxn9xZduOUYHpID0GPRUPkxlQl-MSCWXLYzNiIhTbQIiu4fjJhbFJOpgxmmeskNzuhAJXWNPfKU.webp?r=c6c"
             />
           </Card>
         </div>
-        <div style={{ margin: "0 5px" }}>
-          <Card style={{ width: "15rem" }}>
+        <div className="slider__element">
+          <Card className="slider__element--child">
             <Card.Img
               variant="top"
               src="http://occ-0-6661-55.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABdMUFn_qx4wIfV8BL5FfRORBtxn9xZduOUYHpID0GPRUPkxlQl-MSCWXLYzNiIhTbQIiu4fjJhbFJOpgxmmeskNzuhAJXWNPfKU.webp?r=c6c"
             />
           </Card>
         </div>
-        <div style={{ margin: "0 5px" }}>
-          <Card style={{ width: "15rem" }}>
+        <div className="slider__element">
+          <Card className="slider__element--child">
             <Card.Img
               variant="top"
               src="http://occ-0-6661-55.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABdMUFn_qx4wIfV8BL5FfRORBtxn9xZduOUYHpID0GPRUPkxlQl-MSCWXLYzNiIhTbQIiu4fjJhbFJOpgxmmeskNzuhAJXWNPfKU.webp?r=c6c"
             />
           </Card>
         </div>
-        <div style={{ margin: "0 5px" }}>
-          <Card style={{ width: "15rem" }}>
+        <div className="slider__element">
+          <Card className="slider__element--child">
             <Card.Img
               variant="top"
               src="http://occ-0-6661-55.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABdMUFn_qx4wIfV8BL5FfRORBtxn9xZduOUYHpID0GPRUPkxlQl-MSCWXLYzNiIhTbQIiu4fjJhbFJOpgxmmeskNzuhAJXWNPfKU.webp?r=c6c"
             />
           </Card>
         </div>
-        <div style={{ margin: "0 5px" }}>
-          <Card style={{ width: "15rem" }}>
+        <div className="slider__element">
+          <Card className="slider__element--child">
             <Card.Img
               variant="top"
               src="http://occ-0-6661-55.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABdMUFn_qx4wIfV8BL5FfRORBtxn9xZduOUYHpID0GPRUPkxlQl-MSCWXLYzNiIhTbQIiu4fjJhbFJOpgxmmeskNzuhAJXWNPfKU.webp?r=c6c"
